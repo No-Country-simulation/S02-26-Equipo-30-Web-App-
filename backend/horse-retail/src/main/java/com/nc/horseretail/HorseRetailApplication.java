905b2a260c9a05ecd8f21ff20f4310213b64bc87
Cristian Hoffmann <cristianhof@gmail.com>
2026-02-04 11:16:36 +0100

    add base spring project

---
+++ backend/horse-retail/src/main/java/com/nc/horseretail/HorseRetailApplication.java
@@ -0,0 +1,13 @@
+package com.nc.horseretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseRetailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorseRetailApplication.class, args);
+    }
+
+}
---
+++ backend/horse-retail/src/test/java/com/nc/horseretail/HorseRetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nc.horseretail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseRetailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
